4dbab70f7028102d12430611794cf9471616be85
Asheesh Kumar <asheesh220720@gmail.com>
2024-08-18 01:34:04 +0530

    signup confirmation page layout

--- lib/views/signup_five.dart
+++ lib/views/signup_five.dart
@@ -17,21 +17,95 @@ class _SignupFiveState extends State<SignupFive> {
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 15),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 40,
+            const SizedBox(
+              height: 70,
             ),
-            SvgPicture.asset(
-              'assets/images/tick.svg',
-              height: 100,
-              width: 100,
-              color: Colors.blue,
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/tick.svg',
+                height: 80.41,
+                width: 120.61,
+              ),
             ),
-            SizedBox(
-              height: 10,
+            const SizedBox(
+              height: 30,
             ),
-            Text("You're all done !")
+            const Text(
+              "You're all done!",
+              style: TextStyle(
+                fontFamily: 'BeVietnam',
+                color: Colors.black,
+                fontSize: 32,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Center(
+              child: Text(
+                "Hang tight!  We are currently reviewing your account and",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'BeVietnam',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color.fromARGB(255, 137, 137, 137),
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                "will follow up with you in 2-3 business days. In the",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'BeVietnam',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color.fromARGB(255, 137, 137, 137),
+                ),
+              ),
+            ),
+            const Center(
+              child: Text(
+                " meantime, you can setup your inventory.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'BeVietnam',
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                  color: Color.fromARGB(255, 137, 137, 137),
+                ),
+              ),
+            ),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 0),
+              child: Container(
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(5)),
+                width: 330,
+                height: 52,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 213, 113, 91),
+                    elevation: 0,
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Got it!',
+                    style: TextStyle(
+                      fontFamily: 'assets/fonts/BeVietnam-ExtraBold.ttf',
+                      color: Color.fromRGBO(255, 255, 255, 1),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       )),
